f4758f5d758c2f87d7e022d402d9813ea972fd84
Ahmad Zafar Khan <ahmadzafar224@gmail.com>
2025-02-04 18:50:19 +0500

    Final Commit 04/02/2025

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import BookingWidget from './BookingWidget';
+import BookingWidget from "./BookingWidget";
 
 import {
   Check,
